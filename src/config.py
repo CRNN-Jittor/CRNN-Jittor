89d32ddc55ba4fe4fa0658791ba56659d14aed0c
YS <yuziyang20@mails.tsinghua.edu.cn>
2021-11-19 14:13:50 +0800

    change the directory of dataset

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 common_config = {
     'data_dir': '../data/mnt/ramdisk/max/90kDICT32px/',
-    'test_dir': '../data/mnt/ramdisk/dataset',
+    'test_dir': '../data/',
     'img_width': 100,
     'img_height': 32,
     'map_to_seq_hidden': 64,
