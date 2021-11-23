99b350f54afce846ab737900b2cf7f84a88bfc81
YS <yuziyang20@mails.tsinghua.edu.cn>
2021-11-23 15:27:30 +0800

    add SVT dataset

--- src/datasets.py
+++ src/datasets.py
@@ -558,3 +558,103 @@ class IC15(Dataset):
         targets = jt.stack(targets, dim=0)
 
         return images, targets, target_lengths
+
+
+class SVT(Dataset):
+    def __init__(self,
+                 root_dir,
+                 mode,
+                 img_height=32,
+                 img_width=100,
+                 batch_size=16,
+                 shuffle=False,
+                 drop_last=False,
+                 num_workers=0,
+                 buffer_size=536870912,
+                 stop_grad=True,
+                 keep_numpy_array=False,
+                 endless=False):
+        super().__init__(batch_size=batch_size,
+                         shuffle=shuffle,
+                         drop_last=drop_last,
+                         num_workers=num_workers,
+                         buffer_size=buffer_size,
+                         stop_grad=stop_grad,
+                         keep_numpy_array=keep_numpy_array,
+                         endless=endless)
+        self.img_paths, self.sections, self.texts = self._load_from_raw_files(root_dir, mode)
+        self.total_len = len(self.img_paths)
+        self.img_height = img_height
+        self.img_width = img_width
+
+    def _load_from_raw_files(self, root_dir, mode):
+
+        image_paths = []
+        sections = []
+        texts = []
+
+        paths_file = os.path.join(root_dir, "{}.xml".format(mode))
+
+        with open(paths_file, 'r', encoding='ISO-8859-1') as fr:
+            soup = bs(fr, 'html.parser')
+            imageNames = soup.select('imageName')
+            taggedRectangles_list = soup.select('taggedRectangles')
+            for idx in range(len(imageNames)):
+                imageName = str(imageNames[idx].text)
+                image_path = os.path.join(root_dir, imageName)
+                taggedRectangles = taggedRectangles_list[idx]
+                taggedRectangle = taggedRectangles.select('taggedRectangle')
+                for item in taggedRectangle:
+                    x = float(item.get('x'))
+                    y = float(item.get('y'))
+                    height = float(item.get('height'))
+                    width = float(item.get('width'))
+                    tag = item.find("tag")
+                    if (tag == "###"):
+                        continue
+                    image_paths.append(image_path)
+                    sections.append({"x": x, "y": y, "height": height, "width": width})
+                    texts.append(str(tag.text).lower())
+
+        return image_paths, sections, texts
+
+    def __getitem__(self, index):
+        img_path = self.img_paths[index]
+        section = self.sections[index]
+        try:
+            image = Image.open(img_path).convert('L')  # grey-scale
+            box = (section['x'], section['y'], section['x'] + section['width'],
+                   section['y'] + section['height'])
+            image = image.crop(box)
+        except IOError:
+            print('Corrupted image for %d' % index)
+            return self[index + 1]
+
+        image = image.resize((self.img_width, self.img_height), resample=Image.BILINEAR)
+        image = np.array(image)
+        image = image.reshape((1, self.img_height, self.img_width))
+        image = (image / 127.5) - 1.0
+
+        image = jt.float32(image)
+        if self.texts:
+            text = self.texts[index]
+            target = [CHAR2LABEL[c] for c in text if c in CHARS]
+            target_length = [len(target)]
+
+            target = jt.int64(target)
+            target_length = jt.int64(target_length)
+            return image, target, target_length
+        else:
+            return image
+
+    def collate_batch(self, batch):
+        images, targets, target_lengths = zip(*batch)
+        images = jt.stack(images, dim=0)
+
+        target_lengths = jt.concat(target_lengths, dim=0)
+
+        max_target_length = target_lengths.max()
+        targets = [t.reindex([max_target_length.item()], ["i0"]) for t in targets]
+        targets = jt.stack(targets, dim=0)
+
+        return images, targets, target_lengths
