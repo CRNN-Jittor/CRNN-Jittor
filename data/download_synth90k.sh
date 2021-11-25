694e0450f8b6c2963d8896c0bb48746ba8934803
t4rf9 <t4rf9@163.com>
2021-11-25 19:19:23 +0800

    synth90k download through torrent

--- data/download_synth90k.sh
+++ data/download_synth90k.sh
@@ -1,3 +1,3 @@
-wget https://thor.robots.ox.ac.uk/~vgg/data/text/mjsynth.tar.gz
+transmission-cli --download-dir . https://academictorrents.com/download/3d0b4f09080703d2a9c6be50715b46389fdb3af1.torrent
 tar zxf mjsynth.tar.gz
 mv mnt/ramdisk/max/90kDICT32px/ Synth90k
