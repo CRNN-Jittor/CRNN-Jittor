3d6fe1e5202758d3d35646ecfd2d071323295ac5
YS <yuziyang20@mails.tsinghua.edu.cn>
2021-11-26 17:46:59 +0800

    Debug: index out of range

--- src/BKtree.pyx
+++ src/BKtree.pyx
@@ -21,8 +21,8 @@ def edit_dist(word1, word2):
         dp[i, 0] = i
     for j in range(len2 + 1):
         dp[0, j] = j
-    for i in range(len1 + 1):
-        for j in range(len2 + 1):
+    for i in range(1, len1 + 1):
+        for j in range(1, len2 + 1):
             delta = 0 if word1[i - 1] == word2[j - 1] else 1
             dp[i, j] = min(dp[i - 1, j - 1] + delta, min(dp[i - 1, j] + 1, dp[i, j - 1] + 1))
     return int(dp[len1, len2])
