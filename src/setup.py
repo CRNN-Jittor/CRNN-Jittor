3a34236cf2fde3bb67ea3fc75f67d86672a6a695
t4rf9 <t4rf9@163.com>
2021-12-03 15:50:29 +0800

    change cython source to absolute path

--- src/setup.py
+++ src/setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup, Extension
 from Cython.Build import cythonize
 from Cython.Distutils import build_ext
 import numpy
+import os
+from config import curr_path
 
-extension = Extension(
-    "BKtree",
-    sources=["BKtree.pyx"],
-    include_dirs=[numpy.get_include()],
-    language="c++"
-)
+extension = Extension("BKtree",
+                      sources=[os.path.join(curr_path, "BKtree.pyx")],
+                      include_dirs=[numpy.get_include()],
+                      language="c++")
 
 setup(
     cmdclass={'build_ext': build_ext},
